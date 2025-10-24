e91a895159245e05d783770624ad61915cced6c0
Deviance <ianrafaelabenido@gmail.com>
2025-10-24 14:56:59 +0800

    Updated broadcasting.php

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -29,35 +29,18 @@ return [
     */
 
     'connections' => [
-
-        'reverb' => [
-            'driver' => 'reverb',
-            'key' => env('REVERB_APP_KEY'),
-            'secret' => env('REVERB_APP_SECRET'),
-            'app_id' => env('REVERB_APP_ID'),
-            'options' => [
-                'host' => env('REVERB_HOST'),
-                'port' => env('REVERB_PORT', 443),
-                'scheme' => env('REVERB_SCHEME', 'https'),
-                'useTLS' => env('REVERB_SCHEME', 'https') === 'https',
-            ],
-            'client_options' => [
-                // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
-            ],
-        ],
-
         'pusher' => [
             'driver' => 'pusher',
-            'key' => env('VITE_PUSHER_APP_KEY'),
-            'secret' => env('VITE_PUSHER_APP_SECRET'),
-            'app_id' => env('VITE_PUSHER_APP_ID'),
+            'key' => env('PUSHER_APP_KEY'),
+            'secret' => env('PUSHER_APP_SECRET'),
+            'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'cluster' => env('VITE_PUSHER_APP_CLUSTER'),
-                'host' => env('VITE_PUSHER_HOST') ?: 'api-'.env('VITE_PUSHER_APP_CLUSTER', 'mt1').'.pusher.com',
-                'port' => env('VITE_PUSHER_PORT', 443),
-                'scheme' => env('VITE_PUSHER_SCHEME', 'https'),
+                'cluster' => env('PUSHER_APP_CLUSTER'),
+                'host' => env('PUSHER_HOST') ?: 'api-'.env('PUSHER_APP_CLUSTER', 'mt1').'.pusher.com',
+                'port' => env('PUSHER_PORT', 443),
+                'scheme' => env('PUSHER_SCHEME', 'https'),
                 'encrypted' => true,
-                'useTLS' => env('VITE_PUSHER_SCHEME', 'https') === 'https',
+                'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
             ],
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
