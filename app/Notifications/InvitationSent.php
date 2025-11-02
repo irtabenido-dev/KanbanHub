71c74a9f36797c64105a2e629ac0c33d277b51dc
Deviance <ianrafaelabenido@gmail.com>
2025-11-02 18:22:59 +0800

    commented out the mail option for debugging purposes

--- app/Notifications/InvitationSent.php
+++ app/Notifications/InvitationSent.php
@@ -42,22 +42,23 @@ class InvitationSent extends Notification implements ShouldQueue
     public function via(object $notifiable): array
     {
 
-        return ['mail', 'database'];
+        // return ['mail', 'database'];
+        return ['database'];
     }
 
     /**
      * Get the mail representation of the notification.
      */
-    public function toMail(object $notifiable): MailMessage
-    {
+    // public function toMail(object $notifiable): MailMessage
+    // {
 
-        return (new MailMessage)
-        ->subject("You've been invited to a workspace!")
-        ->greeting("Hello {$this->invited_name}!")
-        ->line("You have been invited to join the {$this->workspace_name} workspace.")
-        ->action("Click this link to accept the invitation", $this->invitationData['invitation_link'])
-        ->salutation("Best regards from {$this->inviter_name} and the {$this->workspace_name} team!");
-    }
+    //     return (new MailMessage)
+    //     ->subject("You've been invited to a workspace!")
+    //     ->greeting("Hello {$this->invited_name}!")
+    //     ->line("You have been invited to join the {$this->workspace_name} workspace.")
+    //     ->action("Click this link to accept the invitation", $this->invitationData['invitation_link'])
+    //     ->salutation("Best regards from {$this->inviter_name} and the {$this->workspace_name} team!");
+    // }
 
     /**
      * Get the array representation of the notification.
