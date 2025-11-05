b55ad5e33b0a9933f6acf9dace4c6e8b64472492
Deviance <ianrafaelabenido@gmail.com>
2025-11-05 23:19:37 +0800

    Added SSL certificate for mysql service

--- config/database.php
+++ config/database.php
@@ -59,7 +59,7 @@ return [
             'engine' => null,
             'timezone' => '+00:00',
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_CA => storage_path('certs/ca.pem'),
             ]) : [],
         ],
 
